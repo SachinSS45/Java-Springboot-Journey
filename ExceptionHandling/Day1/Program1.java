140f15326e816e73976b6a3c90abd77b45f7911f
Sachin Shetkar <ssshetkar2001@gmail.com>
2025-02-12 17:34:44 +0530

    Introduction Of Exception Handling

---
+++ ExceptionHandling/Day1/Program1.java
@@ -0,0 +1,13 @@
+public class Program1{
+
+	public static void main(String[] args){
+
+		//1.Write a code to get the data from file called "sachin.txt"
+
+		//2.read the data from a file
+
+
+		//3.close the file after the reading the content
+
+	}
+}
