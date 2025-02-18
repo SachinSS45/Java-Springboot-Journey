766df5ca0ba8a299bb9f80a108b8e484950df3c5
Sachin Shetkar <ssshetkar2001@gmail.com>
2025-02-18 14:26:01 +0530

    Update Program04.java

--- ExceptionHandling/Day4/Program04.java
+++ ExceptionHandling/Day4/Program04.java
@@ -1,13 +1,20 @@
 /* This code will tell about : 
- *
+ * If checked exception occurs it should be handled or declared to be thrown
  *
  *
  */
 
-public class Program{
+public class Program04{
 
-	public static void main(String[] args){
+	public static void main(String[] args)throws InterruptedException{
 
+		doStuff();
+	}
 
+	public static void doStuff()throws InterruptedException{
+		doMoreStuff();
+	}
+	public static void doMoreStuff() throws InterruptedException{ //throwing to caller
+		Thread.sleep(1000);//checked exception method call
 	}
 }
