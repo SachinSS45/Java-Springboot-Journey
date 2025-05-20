b55c5d49bec69a2f58c0055b8148bffb72b29a65
Sachin Shetkar <ssshetkar2001@gmail.com>
2025-05-20 12:38:21 +0530

    List :: LinkedList

---
+++ CollectionFramework/CollectionDemo/ListDemo/LinkedListDemo/Program01.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class Program01 {
+    public static void main(String[] args) {
+        //Underlying DataStructure :: doubly linked list
+        LinkedList ll = new LinkedList();
+        ll.add("Sachin");
+        ll.add(30);
+        ll.add("shetkar");
+        ll.add(2,"Hero");
+        System.out.println(ll);//[Sachin, 30, Hero, shetkar]
+
+        //to add at first : addFirst(Object o)::void
+        ll.addFirst("Mr.");
+        //to set at particular index 
+        ll.set(2,"Surayakant");
+        System.out.println(ll);
+    }
+}
